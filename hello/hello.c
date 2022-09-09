47b30475fef0b4c0dca688daf003fc720192885f
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-09-09 18:33:09 +0000

    Fri, Sep 9, 2022, 6:33 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
 int main(void)
+
 {
-    printf("hello, world\n");
-}
-{
-    string name = get_string("what is your name: ");
-    printf("hello, %s\n", name);
+    string name = get_string("what's your name: ");
+    printf("hello, %s\n", name);ma
 }
