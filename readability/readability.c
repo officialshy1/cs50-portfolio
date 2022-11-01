d25cd47f7c942f281cc6e4dcc80162cbbf6dd53e
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-01 18:32:16 +0000

    Tue, Nov 1, 2022, 6:32 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -19,5 +19,21 @@ for(int i = 0; i < strlen(text); i++)
 // words with spaces
 int words = 1;
 for(int i = 0; i < strlen(text); i++)
-
+{
+    if(text[i] == ' ')
+    words++;
+}
+// sentences with special charatcers
+int sentences = 0;
+for(int i = 0; i < strlen(text); i++)
+{
+    if(text[i] == '.' || text[i] == '!' ||
+    text[i] = '?')
+    sentences++;
+}
+// coleman index
+float calculation = (0.0588 * letters/words *100) -
+(0.296 * sentences/words * 100) - 15.8;
+int index = round(calculation);
+if(index < 1)
 }
