ded35a55c5384cdbc53c0a90ce7e7c50b6d00a4a
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-02 21:26:14 +0000

    Wed, Nov 2, 2022, 9:26 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -30,7 +30,7 @@ printf("%i words\n", words);
 
 //count spaces between sentences
 int sentences = 0;
-for(int i = 0, i < strlen(text); i++)
+for(int i = 0; i < strlen(text); i++)
 {
     if(text[i] == '.' || text[i] == '!' ||
     text[i] == '?')
