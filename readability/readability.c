16e5776258ae6a162b889d1b806af3c8f85aa509
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-02 21:08:28 +0000

    Wed, Nov 2, 2022, 9:08 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -27,5 +27,15 @@ for(int i = 0; i strlen(text); i++)
     words++;
 }
 printf("%i words\n", words);
+
+//count spaces between sentences
+int sentences = 0;
+for(int i = 0, i < strlen(text); i++)
+{
+    if(text[i] == '.' || text[i] == '!' ||
+    text[i] == '?')
+    sentences++;
+}
+printf("%i sentences\n", sentences);
 }
 
