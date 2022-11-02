d4c5a96f2332988d68729fcb7f6e5199ef304369
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-02 17:38:20 +0000

    Wed, Nov 2, 2022, 5:38 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,5 +8,15 @@ int main(void)
 // ask user for response
 string text = get_string("Text: ");
 printf("%s\n", text);
+
+// number of letters in text
+int letters = 0;
+for(int i = 0; i < strlen(text); i++)
+{
+    if((text[i] >= 'a' && text[i] <= 'z') ||
+    (text[i] >= 'A' && text[i] <= 'Z'))
+    letters++;
+}
+printf("%i letters", letters);
 }
 
