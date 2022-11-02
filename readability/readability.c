3cd651f91fa0551ac5c960cc28649ee6dde1a6e3
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-02 21:22:09 +0000

    Wed, Nov 2, 2022, 9:22 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,58 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
-int main(void) 
+int main(void)
 {
+// ask user for response
+string text = get_string("Text: ");
+printf("%s\n", text);
 
+// number of letters in text
+int letters = 0;
+for(int i = 0; i < strlen(text); i++)
+{
+    if((text[i] >= 'a' && text[i] <= 'z') ||
+    (text[i] >= 'A' && text[i] <= 'Z'))
+    letters++;
+}
+printf("%i letters\n", letters);
+
+// number of word counts
+int words = 1;
+for(int i = 0; i strlen(text); i++)
+{
+    if(text[i] == ' ')
+    words++;
+}
+printf("%i words\n", words);
+
+//count spaces between sentences
+int sentences = 0;
+for(int i = 0, i < strlen(text); i++)
+{
+    if(text[i] == '.' || text[i] == '!' ||
+    text[i] == '?')
+    sentences++;
+}
+printf("%i sentences\n", sentences);
+
+//coleman linux
+float calculation = (0.0588 * letters / words * 100) - (0.296 * sentences / words * 100) - 15.8;
+int index = round(calculation);
+if(index < 1)
+{
+    printf("Before Grade 1\n");
+    return 0;
+}
+else if (index >= 16)
+{
+    printf("Grade 16+\n");
+    return 0;
+}
+else
+{
+    printf("Grade %i\n", index);
+}
 }
