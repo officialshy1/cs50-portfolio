783d6040cf4f7680e110e240732c4bf4399583f1
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-02 17:25:49 +0000

    Wed, Nov 2, 2022, 5:25 PM +00:00

---
+++ readability/readability.c
@@ -0,0 +1,52 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+// ask user for response
+string text = get_string("Text: ");
+
+//count letters in text
+int letters = 0;
+for(int i = 0; i < strlen(text); i++)
+{
+    if((text[i] >= 'a' && text [i]<= 'z')||
+    (text [i] >= 'A' && text[i] <= 'Z'))
+    letters++;
+}
+// words with spaces
+int words = 1;
+for(int i = 0; i < strlen(text); i++)
+{
+    if(text[i] == ' ')
+    words++;
+}
+// sentences with special charatcers
+int sentences = 0;
+for(int i = 0; i < strlen(text); i++)
+{
+    if(text[i] == '.' || text[i] == '!' ||
+    text[i] = '?')
+    sentences++;
+}
+// coleman index
+float calculation = (0.0588 * letters/words *100) -
+(0.296 * sentences/words * 100) - 15.8;
+int index = round(calculation);
+if(index < 1)
+{
+    printf("Before Grade 1\n;)
+    return 0;
+}
+else if (index >= 16)
+{
+    printf("Grade 16+\n");
+    return 0;
+}
+else
+{
+    printf("Grade %i\n", index)
+}
+}
