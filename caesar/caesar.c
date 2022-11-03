a98282ea809352752261906624de5c2762d9b737
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-03 17:07:40 +0000

    Thu, Nov 3, 2022, 5:07 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    // one command line argument
+    if(argc !=2)
+    {
+        printf("usage: ./caesar key\n");
+        return1;
+    }
+    else printf("Success!\n");
+
+    //key
+    string key = argv[i];
+
+    //input is digit
 
 }
