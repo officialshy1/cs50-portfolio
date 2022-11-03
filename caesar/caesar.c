4351aee241f7d669565b831ade954689a0f989ec
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-03 18:23:54 +0000

    Thu, Nov 3, 2022, 6:23 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -18,6 +18,37 @@ int main(int argc, string argv[])
     string key = argv[i];
 
     //input is digit
-    for (int i )
+    for (int i = 0; i < strlen(argv[i]); i++)
+    {
+        if(!isdigit(argv[i][i]))
+        {
+            printf("usage: ./caesar key\n");
+            return 1;
+        }
+    }
+          printf("Success\n%s\n", key);
 
+          //plaintext
+          string plaintext = get_string("Plaintext: ");
+
+          //key to integer
+          int k = atoi(key);
+
+          //ciphertext
+          for(int i = 0; i < strlen(plaintext); i++)
+          {
+            if(isupper(plaintext[i]))
+            {
+                printf("%c", (((plaintext[i]-65)+k)%26)+65);
+            }
+            else if(islower(plaintext[i]))
+            {
+                printf("%c", (((plaintext[i]-97)+k)%26)+97);
+            }
+            else
+            {
+                printf("%c", plaintext[i]);
+            }
+          }
+          printf("\n");
 }
