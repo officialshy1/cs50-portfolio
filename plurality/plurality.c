b718cedd6e6597aeea98b8e201be23657f915971
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-13 20:46:11 +0000

    Sun, Nov 13, 2022, 8:46 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -67,7 +67,14 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if(strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
