5b01afc03a428b80818700d40540e4fe3fd51500
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-16 17:10:42 +0000

    Wed, Nov 16, 2022, 5:10 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,7 +128,10 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
-    
+    for (int i =0; i < candidate_count; i++)
+    {
+        if(strcmp(candidate[i].name, name) == 0)
+    }
     return false;
 }
 
