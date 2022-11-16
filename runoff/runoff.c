2ac018aff4c8fe7372778a78a64db10964c250a4
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-16 18:33:36 +0000

    Wed, Nov 16, 2022, 6:33 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -215,6 +215,12 @@ bool is_tie(int min)
 void eliminate(int min)
 {
     // TODO
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == min)
+        {
+            candidates[i].elimnated == true;
+        }
+    }
     return;
 }
