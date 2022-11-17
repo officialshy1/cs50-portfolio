a187cd0df03ead8d6720ca0acf3cd7520b2e255b
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-17 15:47:13 +0000

    Thu, Nov 17, 2022, 3:47 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -220,7 +220,7 @@ void eliminate(int min)
     {
         if (candidates[i].votes == min)
         {
-            candidates[i].eliminated == true;
+            candidates[i].eliminated = true;
         }
     }
     return;
