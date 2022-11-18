7047b057596bf0f76ea6b04819b8bf08e7482e79
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-18 17:00:13 +0000

    Fri, Nov 18, 2022, 5:00 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -19,13 +19,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     int sRed, sGreen, sBlue;
-    for  (int row = 0; row < height; row++)
+    for  (int i = 0; i < height; i++)
     {
-        for (int col = 0; col < width; col++)
+        for (int j = 0; j < width; j++)
         {
-            sRed = round(0.393 * image[row][col].rgbtRed + 0.769 * image[row][col].rgbtGreen + 0.189 * image[row][col].rgbtBlue);
-            sGreen = round(0.349 * image[row][col].rgbtRed + 0.686 * image[row][col].rgbtGreen + 0.168 * image[row][col].rgbtBlue);
-            sBlue = round(0.272 * image[row][col].rgbtRed + 0.534 * image[row][col].rgbtGreen + 0.131 * image[row][col].rgbtBlue);
+            int sepiaBlue = round(0.393 * image[row][col].rgbtRed + 0.769 * image[row][col].rgbtGreen + 0.189 * image[row][col].rgbtBlue);
+            int sepiaGreen = round(0.349 * image[row][col].rgbtRed + 0.686 * image[row][col].rgbtGreen + 0.168 * image[row][col].rgbtBlue);
+            int sepiaBlue = round(0.272 * image[row][col].rgbtRed + 0.534 * image[row][col].rgbtGreen + 0.131 * image[row][col].rgbtBlue);
             image[row][col].rgbtRed = sRed > 255? 255: sRed;
             image[row][col].rgbtGreen = sRed > 255? 255: sGreen;
             image[row][col].rgbtBlue = sRed > 255? 255: sBlue;
