506fc004cb1896c2b1ab4924695ef76c189f03e2
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-18 16:44:24 +0000

    Fri, Nov 18, 2022, 4:44 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -23,9 +23,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int col = 0; col < width; col++)
         {
-            sRed = 0.393 * image[row][col].rgbtRed + 0.769 * image[row][col].rgbtGreen + 0.189 * image[row][col].rgbtBlue;
-            sGreen = 0.349 * image[row][col].rgbtRed + 0.686 * image[row][col].rgbtGreen + 0.168 * image[row][col].rgbtBlue;
-            sBlue = 0.272 * image[row][col].rgbtRed + 0.534 * image[row][col].rgbtGreen + 0.131 * image[row][col].rgbtBlue;
+            sRed = round(0.393 * image[row][col].rgbtRed + 0.769 * image[row][col].rgbtGreen + 0.189 * image[row][col].rgbtBlue);
+            sGreen = round(0.349 * image[row][col].rgbtRed + 0.686 * image[row][col].rgbtGreen + 0.168 * image[row][col].rgbtBlue);
+            sBlue = round(0.272 * image[row][col].rgbtRed + 0.534 * image[row][col].rgbtGreen + 0.131 * image[row][col].rgbtBlue);
         }
     }
     return;
