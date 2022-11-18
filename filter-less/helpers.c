73c784e1bcc3cb7bc55b880daa420ca27c809576
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-18 20:14:39 +0000

    Fri, Nov 18, 2022, 8:14 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -18,46 +18,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    int originalRed, originalGreen, originalBlue;
-    int sepiaRed, sepiaBlue, sepiaGreen;
-
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            // assigning value to my variables
-            originalRed = image[i][j].rgbtRed;
-            originalGreen = image[i][j].rgbtGreen;
-            originalBlue = image[i][j].rgbtBlue;
+            int sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+            int sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            int sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
 
-            //calcualtion sepia
-            sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
-            if (sepiaRed > 255)
-            {
-                image[i][j].rgbtRed = 255;
-            }
-            else
-            {
-                image[i][j].rgbtRed = sepiaRed;
-            }
-            sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
-            if (sepiaGreen > 255)
-            {
-                image[i][j].rgbtGreen = 255;
-            }
-            else
-            {
-                image[i][j].rgbtGreen = sepiaGreen;
-            }
-            sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
-            if (sepiaBlue > 255)
-            {
-                image[i][j].rgbtBlue = 255;
-            }
-            else
-            {
-                image[i][j].rgbtBlue = sepiaBlue;
-            }
+            image[i][j].rgbtBlue = (sepiaBlue > 255) ? 255 : sepiaBlue;
+            image[i][j].rgbtGreen = (sepiaGreen > 255) ? 255 : sepiaGreen;
+            image[i][j].rgbtRed = (sepiaRed > 255) ? 255 : sepiaRed;
         }
     }
     return;
