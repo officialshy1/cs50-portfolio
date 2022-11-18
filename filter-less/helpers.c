b469610c8afc441a48795f024a27d0f7a87139c9
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-18 16:27:59 +0000

    Fri, Nov 18, 2022, 4:27 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -18,6 +18,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    
     return;
 }
 
