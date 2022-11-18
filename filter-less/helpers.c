b975cc51b7bf03c55056a1d2300288ecc09f4cbe
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-11-18 19:32:38 +0000

    Fri, Nov 18, 2022, 7:32 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -37,7 +37,33 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             originalBlue = image[i][j].rgbtBlue;
 
             //calcualtion sepia
-            
+            sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            if (sepiaRed > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
+            else
+            {
+                image[i][j].rgbtRed = sepiaRed;
+            }
+            sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            if (sepiaGreen > 255)
+            {
+                image[i][j].rgbtGreen = 255;
+            }
+            else
+            {
+                image[i][j].rgbtGreen = sepiaGreen;
+            }
+            sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+            if (sepiaBlue > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
+            else
+            {
+                image[i][j].rgbtBlue = sepiaBlue;
+            }
         }
     }
     return;
@@ -46,6 +72,20 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (width/2); j++)
+        {
+            int temPred = image[i][j].rgbtRed;
+            int temPred = image[i][j].rgbtGreen;
+            int temPred = image[i][j].rgbtBlue;
+
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+
+        }
+    }
     return;
 }
 
