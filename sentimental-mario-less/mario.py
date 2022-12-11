d934b7c0ba88af226153f409642389a0c60408c3
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-12-11 18:14:01 +0000

    Sun, Dec 11, 2022, 6:14 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,15 +3,14 @@
 from cs50 import get_int
 
 while True:
-    height = get_int("Height: ")
-    if height > 0 and height < 9:
+    n = get_int("Height: ")
+    if n > 0 and n < 9:
         break
-for row in range(height):
-    for space in range(height - row - 1, 0, -1):
-        print(" ", end="")
-    for hash in range(row + 1):
-        print("#", end ="")
-    print("  ", end="")
-    for right_hash in range(row+1):
-        print("#", end ="")
-    print("\n", end ="")
+
+for i in range(0, n, 1):
+    for j in range(0, n, 1):
+        if (i+j < n-1):
+            print("")
+        else:
+            print("#")
+      print()
