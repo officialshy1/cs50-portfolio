01a2b71fd66da26e13167f591c4193b5d94fd279
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-12-11 18:17:11 +0000

    Sun, Dec 11, 2022, 6:17 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -10,7 +10,7 @@ while True:
 for i in range(0, n, 1):
     for j in range(0, n, 1):
         if (i+j < n-1):
-            print("")
+            print("", end="")
         else:
-            print("#")
+            print("#", end="")
     print()
