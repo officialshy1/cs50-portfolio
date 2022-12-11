f937e39f32afd81930a84fdc9d55779c767439eb
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2022-12-11 18:05:13 +0000

    Sun, Dec 11, 2022, 6:05 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,1 +1,14 @@
 # TODO
+while True:
+    height = get_int("Height: ")
+    if height > 0 and height < 9:
+        break
+for row in range(height):
+    for space in range(height - row - 1, 0, -1):
+        print(" ", end="")
+    for hash in range(row + 1):
+        print("#", end ="")
+    print("  ", end="")
+    for right_hash in range(row+1):
+        print("#", end ="")
+    print("\n", end ="")
