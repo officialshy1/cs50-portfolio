447582609a597339ef848ff2843817f6e96492b6
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2023-07-22 20:00:09 +0000

    Sat, Jul 22, 2023, 3:00 PM -05:00

--- finance/app.py
+++ finance/app.py
@@ -104,7 +104,14 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    if request.method
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        quote = lookup(symbol)
+        if not quote:
+            return apology("invalid symbol", 400)
+        return render_template("quote.html", quote=quote)
+    else:
+        return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
