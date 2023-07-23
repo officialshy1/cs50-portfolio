14575e177515ccc4cd88b4d07f9a3b7ba960cc87
officialshy1 <105302022+officialshy1@users.noreply.github.com>
2023-07-23 19:05:00 +0000

    Sun, Jul 23, 2023, 2:05 PM -05:00

--- finance/app.py
+++ finance/app.py
@@ -66,7 +66,14 @@ def buy():
         db.execute("UPDATE users SET cash = cash - :total_cost WHERE id = :user_id",
                    total_cost=total_cost, user_id=session["user_id"])
         # add the purchase to the history table
-        db.execute()
+        db.execute("INSERT INTO transactions (user_id, symbol, shares, price) VALUES (:user_id, :symbol, :shares, :price)",
+                   user_id=session["user_id"], symbol=symbol, shares=shares, price=price)
+
+        flash(f"Bought {shares} shares of {symbol} for {usd(total_cost)}!")
+        return redirect("/")
+
+    else:
+        return render_template("buy.html")
 
 
 @app.route("/history")
